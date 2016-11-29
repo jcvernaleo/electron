484e5a4ae09f350e0897054bf20e0f509e021fe5
Cheng Zhao <zcbenz@gmail.com>
2016-11-29 17:01:34 +0900

    Update brightray and libchromiumcontent to Chrome 54

--- script/lib/config.py
+++ script/lib/config.py
@@ -9,7 +9,7 @@ import sys
 BASE_URL = os.getenv('LIBCHROMIUMCONTENT_MIRROR') or \
     'https://s3.amazonaws.com/github-janky-artifacts/libchromiumcontent'
 LIBCHROMIUMCONTENT_COMMIT = os.getenv('LIBCHROMIUMCONTENT_COMMIT') or \
-    'f14fb5fb9cb3c3a57a2ac1a9725fd9373ef043d2'
+    '9578c02202a6050e3fd48dddd5433fbb48089337'
 
 PLATFORM = {
   'cygwin': 'win32',
